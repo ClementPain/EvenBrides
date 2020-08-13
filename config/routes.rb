04a45b41b8ae0a5516c2d84f0eaca477a1d18f20
ClementPain <clementpain@outlook.com>
2020-08-13 21:47:24 +0200

    V4

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get root 'events#index'
+
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
+  
+  resources :events do
+    resources :attendances
+    resources :event_pictures, only: [:create]
+    #permet de créer les routes permettant d'attacher les photos
+  end
+
+
+  resources :charges
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
